4a2b42dcbabcd64e9910c30639d2280680317a6b
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-08-14 14:32:25 +0800

    [fengyi_ling]create spring project

---
+++ src/main/java/com/example/developmentquiz/DevelopmentQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.developmentquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/developmentquiz/DevelopmentQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developmentquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
